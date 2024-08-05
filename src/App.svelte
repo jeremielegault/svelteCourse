96e7b9509984d3b6058a8411fc0fa8459da50e84
Jeremie Legault <jeremie.legault@avanade.com>
2024-08-05 09:30:16 -0400

    add some more svelte code

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	// export let name;
+	// export let age;
+	let age = 5
+	let name = 'Jeremie'
+
+	function incrementAge() {
+		age += 5
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name} and I am {age} years old!</h1>
+<button on:click="{incrementAge}">Change Age</button>
